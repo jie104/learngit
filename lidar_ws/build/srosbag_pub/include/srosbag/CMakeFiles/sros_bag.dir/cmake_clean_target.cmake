f21b2a0b340b410eb4c489fd19de947474f35189
zhengxiaojie <1430974500@qq.com>
2022-08-30 00:29:57 +0800

    add two file

---
+++ lidar_ws/build/srosbag_pub/include/srosbag/CMakeFiles/sros_bag.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/home/zxj/learngit/lidar_ws/devel/lib/libsros_bag.a"
+)
---
+++ lidar_ws/build/wj_716_lidar/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zxj/learngit/lidar_ws/src/wj_716_lidar
+# Build directory: /home/zxj/learngit/lidar_ws/build/wj_716_lidar
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
