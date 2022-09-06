e9cab83aad1c9c91a1f02f7571c4f1cbd9bac95a
zhengxiaojie <1430974500@qq.com>
2022-09-06 09:52:22 +0800

    add tf04_test

---
+++ ROS/demo04_ws/build/tf04_test/CMakeFiles/test02_pub_turtle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/zxj/桌面/learngit/ROS/demo04_ws/devel/lib/tf04_test/test02_pub_turtle"
+  "/home/zxj/桌面/learngit/ROS/demo04_ws/devel/lib/tf04_test/test02_pub_turtle.pdb"
+  "CMakeFiles/test02_pub_turtle.dir/src/test02_pub_turtle.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test02_pub_turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
